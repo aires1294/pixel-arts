ad85b28fc553795a217ad4eac7ed6705a93aedfe
aires1294 <pedrotja.engenharia@gmail.com>
2022-07-08 09:39:33 -0300

    item 6 desafio

--- script.js
+++ script.js
@@ -11,3 +11,17 @@ function criaBoard(parametro) {
   }
 }
 criaBoard(5);
+
+function selecaoCor() {
+  const cores = document.querySelectorAll('#color-palette');
+
+  for (let index = 0; index < cores.length; index += 1) {
+    cores[index].addEventListener('click', function (event) {
+      const selecaoAtual = document.querySelector('.selected');
+      selecaoAtual.classList.remove('selected');
+      event.target.classList.add('selected');
+    });
+  }
+}
+
+selecaoCor();
