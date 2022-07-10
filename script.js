0d50ead610635a593939116a5fb0d724da10394b
aires1294 <pedrotja.engenharia@gmail.com>
2022-07-09 21:59:02 -0300

    item 9

--- script.js
+++ script.js
@@ -50,19 +50,11 @@ function preparaQuadro() {
 }
 preparaQuadro();*/
 
-
-
-
-
-
-
-
 const pixelBoard = document.getElementById('pixel-board');
 
 const classeDaSelecaoAtual = document.getElementsByClassName('selected');
 const novoPixelBoard = document.getElementsByClassName('pixel');
 const paleta = document.querySelectorAll('.color');
-
 
 function criaBoard(parametro) {
   for (let indexLinha = 0; indexLinha < parametro; indexLinha += 1) {
@@ -70,6 +62,7 @@ function criaBoard(parametro) {
       const pixelElement = document.createElement('div');
       pixelElement.classList.add('pixel');
       pixelBoard.appendChild(pixelElement);
+      pixelElement.style.backgroundColor = 'white';
     }
     const coluna = document.createElement('br');
     pixelBoard.appendChild(coluna);
@@ -82,19 +75,20 @@ function selecaoCor(selecao) {
   selecao.target.classList.add('selected');
 }
 
-
-
-
 function pintarPixel(p) {
   console.log(classeDaSelecaoAtual[0].classList[1]);
   p.target.style.backgroundColor = classeDaSelecaoAtual[0].classList[1];
 }
 
 function preparaQuadro() {
-  for (let jogar = 0; jogar < paleta.length; jogar += 1){
+  for (let jogar = 0; jogar < paleta.length; jogar += 1) {
     paleta[jogar].addEventListener('click', selecaoCor);
   }
-  for (let indexPinta = 0; indexPinta < novoPixelBoard.length; indexPinta += 1) {
+  for (
+    let indexPinta = 0;
+    indexPinta < novoPixelBoard.length;
+    indexPinta += 1
+  ) {
     novoPixelBoard[indexPinta].className = 'pixel';
     novoPixelBoard[indexPinta].addEventListener('click', pintarPixel);
   }
